3711cd4411f6ff0e3300e1a5426818bd68a7bbc5
Tuan Ho <hothanhtuan1209@gmail.com>
2023-12-28 17:50:17 +0700

    Create model employees

---
+++ practice/django_practice/employees/models.py
@@ -0,0 +1,88 @@
+import uuid
+
+from django.db import models
+from enum import Enum
+from datetime import date
+
+from departments.models import Department
+
+
+class ActiveStatus(Enum):
+    ACTIVE = "Active"
+    DISABLED = "Disabled"
+
+
+class Gender(Enum):
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHER = "Other"
+
+
+class Employee(models.Model):
+    """
+    A class representing different employees within the system.
+
+    Attributes:
+        id (UUIDField): The primary key for the employee.
+        first_name (CharField): The first name of the employee.
+        last_name (CharField): The last name of the employee.
+        gender (CharField): The gender of the employee.
+        birthday (DateField): The employee's birth date.
+        email(EmailField): The employee's email.
+        status (CharField): The status of the employee in company.
+        department (ForeignKey): This is the instance of department.
+    """
+
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthday = models.DateField()
+    email = models.EmailField(max_length=100, unique=True)
+    gender = models.CharField(
+        max_length=10,
+        choices=[(gender.value, gender.value) for gender in Gender],
+        default=Gender.MALE.value,
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=[(status.value, status.value) for status in ActiveStatus],
+        default=ActiveStatus.ACTIVE.value,
+    )
+    department = models.ForeignKey(
+        Department, on_delete=models.CASCADE
+    )
+
+    def get_full_name(self):
+        """
+        Combine first name and last name to create the full name of the
+        employee.
+        """
+
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        """
+        Return a string representation of the Employee object.
+        """
+
+        return str(self.get_full_name())
+
+    @classmethod
+    def males_over_25(cls):
+        """
+        Get a list of all male employees older than 25 years old.
+        """
+
+        return cls.objects.filter(
+            gender=Gender.MALE.value, birthday__lt=date.today() - timedelta(days=25 * 365)
+        )
+
+    @classmethod
+    def sale_after_2000(cls):
+        """
+        Get a list of employees in the Sales department and born after 2000.
+        """
+
+        return cls.objects.filter(department__name='Sale', birthday__year__gt=2000)
