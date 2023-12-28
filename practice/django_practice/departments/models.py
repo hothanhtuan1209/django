fb4e976b5be2e2ab58e7fe617a5d4b22d2ddab50
Tuan Ho <hothanhtuan1209@gmail.com>
2023-12-28 17:49:12 +0700

    Create model departments

---
+++ practice/django_practice/departments/models.py
@@ -0,0 +1,27 @@
+import uuid
+from django.db import models
+
+
+class Department(models.Model):
+    """
+    A class representing different departments within the system.
+
+    Attributes:
+        id (UUID): The primary key for the department
+        name (CharField): The name of the department
+        description (TextField): the description of the department
+    """
+
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    name = models.CharField(max_length=100, unique=True),
+    description = models.TextField(max_length=200)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        Return s string representation of the Department object.
+        """
+
+        return str(self.name)
