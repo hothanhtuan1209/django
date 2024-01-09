92e2ccba541c3385a42a1b65aa41cf391b9f8dac
Tuan Ho <hothanhtuan1209@gmail.com>
2024-01-09 14:32:19 +0700

    Update archived and modified_at field for basemodel

--- practice/django_practice/django_practice/models.py
+++ practice/django_practice/django_practice/models.py
@@ -8,12 +8,20 @@ class BaseModel(models.Model):
     time and unique identifier of objects.
 
     Attributes:
-        create_at (DateTimeField): A field to store the creation time of objects.
-        id (UUIDField): A field to store a unique identifier (UUID) for each object.
+        create_at (DateTimeField): A field to store the creation time of
+        objects.
+        id (UUIDField): A field to store a unique identifier (UUID) for each
+        object.
+        archived (BooleanField): A field to indicate whether the object is
+        archived (deleted) or not.
+        modified_at (DateTimeField): A field to store the last modification
+        time of objects.
     """
 
     create_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    archived = models.BooleanField(default=False)
+    modified_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
