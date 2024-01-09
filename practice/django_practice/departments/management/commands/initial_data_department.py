59c56b010e9dbd8136db9134b4ac5041732457dd
Tuan Ho <hothanhtuan1209@gmail.com>
2024-01-09 10:04:49 +0700

    Update bulk create for provide initial for models

--- practice/django_practice/departments/management/commands/initial_data_department.py
+++ practice/django_practice/departments/management/commands/initial_data_department.py
@@ -3,20 +3,24 @@ from departments.models import Department
 
 
 class Command(BaseCommand):
-    help = 'Create initial departments'
+    help = "Create initial departments"
 
     def handle(self, *args, **options):
         departments_data = [
-            {'name': 'Accounting', 'description': 'Accounting department'},
-            {'name': 'IT', 'description': 'Information Technology'},
-            {'name': 'Sale', 'description': 'Sales Department'},
-            {'name': 'HR', 'description': 'Human Resources Department'},
-            {'name': 'Business', 'description': 'Business Department'},
+            {"name": "Accounting", "description": "Accounting department"},
+            {"name": "IT", "description": "Information Technology"},
+            {"name": "Sale", "description": "Sales Department"},
+            {"name": "HR", "description": "Human Resources Department"},
+            {"name": "Business", "description": "Business Department"},
         ]
 
-        for data in departments_data:
-            Department.objects.create(**data)
+        departments_to_create = [
+            Department(name=data["name"], description=data["description"])
+            for data in departments_data
+        ]
+
+        Department.objects.bulk_create(departments_to_create)
 
         self.stdout.write(
-            self.style.SUCCESS('Initial departments created successfully!')
-            )
+            self.style.SUCCESS("Initial departments created successfully!")
+        )
