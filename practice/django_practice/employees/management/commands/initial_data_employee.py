42cd16c17cfc96c21e58de80080d6d92529f1a79
Tuan Ho <hothanhtuan1209@gmail.com>
2024-01-09 11:41:32 +0700

    Update randomchoice to object

--- practice/django_practice/employees/management/commands/initial_data_employee.py
+++ practice/django_practice/employees/management/commands/initial_data_employee.py
@@ -8,12 +8,12 @@ from departments.models import Department
 
 
 class Command(BaseCommand):
-    help = 'Create fake employees for testing'
+    help = "Create fake employees for testing"
 
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS('Creating fake employees...'))
+        self.stdout.write(self.style.SUCCESS("Creating fake employees..."))
 
-        department_names = Department.objects.values_list('name', flat=True)
+        department_names = Department.objects.values_list("name", flat=True)
 
         employees = []
 
@@ -22,12 +22,10 @@ class Command(BaseCommand):
             last_name = Faker().last_name()
             birthday = Faker().date_of_birth(minimum_age=20, maximum_age=60)
             email = Faker().email()
-            gender = random.choice(['Male', 'Female'])
-            status = 'Active'
+            gender = random.choice(["Male", "Female"])
+            status = "Active"
 
-            department_name = random.choice(department_names)
-
-            department = Department.objects.get(name=department_name)
+            department = Department.objects.get(name=random.choice(department_names))
 
             employees.append(
                 Employee(
@@ -37,12 +35,10 @@ class Command(BaseCommand):
                     email=email,
                     gender=gender,
                     status=status,
-                    department=department
+                    department=department,
                 )
             )
 
         Employee.objects.bulk_create(employees)
 
-        self.stdout.write(
-            self.style.SUCCESS('Fake employees created successfully!')
-        )
+        self.stdout.write(self.style.SUCCESS("Fake employees created successfully!"))
