ee45de1939a8459adfcf31f177f42a8a13c0cc9b
Tuan Ho <hothanhtuan1209@gmail.com>
2024-01-09 09:49:59 +0700

    Update docstring for basemodel

--- practice/django_practice/django_practice/models.py
+++ practice/django_practice/django_practice/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 class BaseModel(models.Model):
     """
-    A base model with a 'create_at' field to track the creation
-    time of objects.
+    A base model with 'create_at' and 'id' fields to track the creation
+    time and unique identifier of objects.
+
+    Attributes:
+        create_at (DateTimeField): A field to store the creation time of objects.
+        id (UUIDField): A field to store a unique identifier (UUID) for each object.
     """
 
     create_at = models.DateTimeField(auto_now_add=True)
