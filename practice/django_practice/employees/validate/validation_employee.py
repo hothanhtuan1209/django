56a57e9816b887b5b8ea868aace3a0f64828b8ca
Tuan Ho <hothanhtuan1209@gmail.com>
2024-01-10 14:23:41 +0700

    Update file validate for birthday and email

--- practice/django_practice/employees/validate/validation_employee.py
+++ practice/django_practice/employees/validate/validation_employee.py
@@ -3,12 +3,12 @@ from django.core.exceptions import ValidationError
 from ..utils.number_of_age import calculate_age
 
 
-def validate_age(employee):
+def validate_age(birthday):
     age_limit = 18
-    if calculate_age(date.today(), employee.birthday) < age_limit:
+    if calculate_age(date.today(), birthday) < age_limit:
         raise ValidationError("Employee must be at least 18 years old")
 
 
-def validate_email(employee):
-    if not employee.email or "@" not in employee.email:
+def validate_email(email):
+    if not email or "@" not in email:
         raise ValidationError("Invalid email format")
