7b8baee313c3a362361aadd23c82f95fcf710d6e
Tuan Ho <hothanhtuan1209@gmail.com>
2024-01-15 14:12:54 +0700

    Debug file in employee model

--- practice/django_practice/employees/models.py
+++ practice/django_practice/employees/models.py
@@ -4,7 +4,7 @@ from departments.models import Department
 from django_practice.models import BaseModel
 from django_practice.constants.enum import Gender, ActiveStatus
 from .utils.date_utils import find_date
-from .validate.validation_employee import validate_age, validate_email
+from .validate.validations import validate_age, validate_email
 
 
 class EmployeeQuerySet(models.QuerySet):
