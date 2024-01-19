9a18f3fe8dc276c16a2a6e2e060dd3d1421bf7bd
Tuan Ho <hothanhtuan1209@gmail.com>
2024-01-19 16:41:34 +0700

    Update comment, add sort data, update helper function to filter

--- practice/django_practice/employees/views.py
+++ practice/django_practice/employees/views.py
@@ -3,6 +3,22 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 
 from .models import Employee
+from django_practice.settings import PAGE_SIZE
+
+
+def build_query_filter(name, department):
+    """
+    Helper function to build the query filter based on name and department.
+    """
+    query_filter = Q()
+
+    if name:
+        query_filter |= Q(first_name__icontains=name) | Q(last_name__icontains=name)
+
+    if department:
+        query_filter &= Q(department__name=department)
+
+    return query_filter
 
 
 def employees(request):
@@ -10,26 +26,15 @@ def employees(request):
     This function gets a list of employees from the database.
     """
 
-    employees_queryset = Employee.objects.all()
+    employees_name = request.GET.get("name")
+    department_name = request.GET.get("department")
+    order_by = request.GET.get("order_by", "-created_at")
 
-    # Filter by name
-    employees_name = request.GET.get('name')
-    if employees_name:
-        employees_queryset = employees_queryset.filter(
-            Q(first_name__icontains=employees_name)
-            | Q(last_name__icontains=employees_name)
-        )
+    query_filter = build_query_filter(employees_name, department_name)
+    employees_queryset = Employee.objects.filter(query_filter).order_by(order_by)
 
-    # Filter by department
-    department_name = request.GET.get('department')
-    if department_name:
-        employees_queryset = employees_queryset.filter(
-            department__name=department_name
-        )
-
-    items_in_page = 20
-    paginator = Paginator(employees_queryset, items_in_page)
-    page = request.GET.get('page', 1)
+    paginator = Paginator(employees_queryset, PAGE_SIZE)
+    page = request.GET.get("page", 1)
 
     try:
         employees_page = paginator.page(page)
@@ -39,14 +44,14 @@ def employees(request):
         employees_page = paginator.page(paginator.num_pages)
 
     context = {
-        'employees': [
+        "employees": [
             {
-                'id': employee.id,
-                'full_name': employee.get_full_name(),
-                'department': employee.department.name
+                "id": employee.id,
+                "full_name": employee.get_full_name(),
+                "department": employee.department.name,
             }
             for employee in employees_page
         ]
     }
 
-    return render(request, 'list.html', context)
+    return render(request, "list.html", context)
